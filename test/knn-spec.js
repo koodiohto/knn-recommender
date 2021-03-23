e4b1ecd133178bba0f5a26903c770c4c4c9fc150
koodiohto <orainio@gmail.com>
2021-03-23 09:39:50 +0200

    initial commit

---
+++ test/knn-spec.js
@@ -0,0 +1,20 @@
+import knnRecommender from '../src/knn-recommender.js';
+import { expect } from 'chai';
+
+
+describe('basic test', () => {
+
+    before(() => {
+
+    });
+
+    after(() => {
+    });
+
+    it('should do something', () => {
+        const success = true
+        expect(success).to.equal(true);
+        //expect(firstDocument.patient.id).to.equal("Potilas1");
+        //expect(firstDocument.patient.operationDate.toString()).to.equal(new Date("2020-11-18").toString());
+    })
+})
