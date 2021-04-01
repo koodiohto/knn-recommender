de736506cc188d572c997c02b7ca825bba696c3b
koodiohto <orainio@gmail.com>
2021-04-01 15:09:31 +0300

    Readme updated

--- src/knn-recommender.ts
+++ src/knn-recommender.ts
@@ -159,7 +159,7 @@ export default class KNNRecommender {
 
 
     /**
-     * Update a liking value for a certain user and item.
+     * Update the liking value for a certain user and item.
      * @param userId 
      * @param itemId 
      */
@@ -169,7 +169,7 @@ export default class KNNRecommender {
 
 
     /**
-     * Update a disliking value for a certain user and item.
+     * Update the disliking value for a certain user and item.
      * @param userId 
      * @param itemId 
      */
